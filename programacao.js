5f67957804156a2ac4a6f7fc7b8eecfe2eaab0dc
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-09-18 22:53:59 -0300

    Revisão Metade da Aula 05

--- programacao.js
+++ programacao.js
@@ -1,13 +1,23 @@
 const form = document.getElementById('formulario')
 let linhas = ''
-const feliz = `<img src="imagens/feliz.png" alt="feliz">`
-const triste = `<img src="imagens/triste.png" alt="feliz">`
+const feliz = '<img src="imagens/feliz.png" alt="feliz">'
+const triste = '<img src="imagens/triste.png" alt="triste">'
+const atividades = []
+const notas = []
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e){
   e.preventDefault();
+  adicionaLinha();
+  atualizaTabela();
+  recebeMediaFinal();
+})
 
+function adicionaLinha() {
   const NomeAtividade = document.getElementById('nome-atividade')
   const NotaAtividade = document.getElementById('nota-atividade')
+
+  atividades.push(NomeAtividade.value)
+  notas.push(Number(NotaAtividade.value))
 
   let linha = `<tr>`
   linha += `<td>${NomeAtividade.value}</td>`
@@ -17,9 +27,27 @@ form.addEventListener('submit', function(e) {
 
   linhas += linha
 
+  NomeAtividade.value = ''
+  NotaAtividade.value = ''
+}
+
+function atualizaTabela() {
   const CorpoTabela = document.querySelector('tbody')
   CorpoTabela.innerHTML = linhas
+}
 
-  NomeAtividade.value = '';
-  NotaAtividade.value = '';
-})
+
+function calculaMediaFinal() {
+  let somaDasNotas = 0
+
+  for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
+  }
+  return somaDasNotas / notas.length;
+}
+
+function recebeMediaFinal() {
+  const Media = calculaMediaFinal()
+  document.getElementById('nota-media').innerHTML = Media
+  document.getElementById('resultado-media').innerHTML = Media >= 7 ? 'Aprovado' : 'Reprovado'
+}
