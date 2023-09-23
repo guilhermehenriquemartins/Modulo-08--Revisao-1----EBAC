05e759ff94f509ffcee15a168cfd1a04015d9c0b
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-09-23 10:33:34 -0300

    Conclusão Revisão

--- programacao.js
+++ programacao.js
@@ -1,58 +1,54 @@
 const form = document.getElementById('formulario')
 let linhas = ''
-let feliz = '<img src="imagens/feliz.png" alt="Aprovado">'
-let triste = '<img src="imagens/triste.png" alt="Reprovado">'
-const notas = []
+const feliz = '<img src="imagens/feliz.png" alt="Aprovado">'
+const triste = '<img src="imagens/triste.png" alt="Reprovado">'
 const atividades = []
-const Aprovado = '<span class = "resultado aprovado">Aprovado</span>'
-const Reprovado = '<span class="resultado reprovado">Reprovado</span>'
-const NotaMinima = Number(prompt('Digite a nota mínima: '))
+const notas = []
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e) {
   e.preventDefault();
+
   adicionaLinha();
-  atualizaTabela();
-  AtualizaMedia();
+  calculaMedia();
+  atualizaMedia();
+  
 })
 
 function adicionaLinha() {
   const NomeAtividade = document.getElementById('nome-atividade')
   const NotaAtividade = document.getElementById('nota-atividade')
 
-  if(atividades.includes(NomeAtividade.value)) {
-    alert(`Atividade ${NomeAtividade.value} já foi inserida!`)
+  if (atividades.includes(NomeAtividade.value)) {
+    alert(`Atividade ${NomeAtividade.value} já está incluída!`)
   } else {
-    notas.push(Number(NotaAtividade.value))
     atividades.push(NomeAtividade.value)
+    notas.push(Number(NotaAtividade.value))
 
     let linha = `<tr>`
-    linha += `<td>${NomeAtividade.value}</td>`
+    linha+= `<td>${NomeAtividade.value}</td>`
     linha += `<td>${NotaAtividade.value}</td>`
-    linha += `<td>${NotaAtividade.value >= NotaMinima ? feliz : triste}</td>`
+    linha += `<td>${NotaAtividade.value >= 7 ? feliz : triste}</td>`
     linha += `</tr>`
 
     linhas += linha
+
+    const CorpoTabela = document.querySelector('tbody')
+    CorpoTabela.innerHTML = linhas
   }
-
-  NomeAtividade.value = ''
-  NotaAtividade.value = ''
 }
 
-function atualizaTabela() {
-  const CorpoTabela = document.querySelector('tbody')
-  CorpoTabela.innerHTML = linhas
-}
-
-function CalculaMedia() {
-  let SomaMedia = 0
-  for (let i = 0; i < notas.length; i++ ) {
-    SomaMedia += notas[i]
+function calculaMedia() {
+  let MediaFinal = 0
+  for(let i = 0; i < notas.length; i++) {
+    MediaFinal += notas[i]
   }
-  return SomaMedia / notas.length
+  return MediaFinal / notas.length
 }
 
-function AtualizaMedia() {
-  const RecebeMedia = CalculaMedia()
-  document.getElementById('nota-media').innerHTML = RecebeMedia
-  document.getElementById('resultado-media').innerHTML = RecebeMedia >= NotaMinima ? Aprovado : Reprovado
+function atualizaMedia() {
+  const MediaNotas = calculaMedia()
+  document.getElementById('nota-media').innerHTML = MediaNotas
+  document.getElementById('resultado-media').innerHTML = MediaNotas >= 7 ? spanAprovado : spanReprovado
 }
